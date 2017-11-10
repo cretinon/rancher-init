f0947c385bc58c9e03bd4b8ff200850cc9a08477
root <root@vps3.localdomain>
2017-11-10 16:52:33 +0000

    yep

--- setup_master.sh
+++ setup_master.sh
@@ -20,14 +20,24 @@ mkfs.ext4 /dev/glustervg/glusterlv1
 echo "/dev/glustervg/glusterlv1 /glusterfs ext4 defaults 0 0" >> /etc/fstab
 mount /glusterfs
 
+mkdir /docker
+gluster volume create docker replica 2 transport tcp vps3.cretinon.fr:/glusterfs/$VOL vps4.cretinon.fr:/glusterfs/$VOL force
+while [ $? -eq 1 ]; do sleep 10 ; gluster volume create docker replica 2 transport tcp vps3.cretinon.fr:/glusterfs/$VOL vps4.cretinon.fr:/glusterfs/$VOL force ; done
+echo "localhost:/docker /docker glusterfs defaults,_netdev 0 0" >> /etc/fstab
+mount /docker
+
 mkdir -p /docker/git_clone
 cd /docker/git_clone
 git clone https://github.com/cretinon/rancher-init.git
 
+mkdir -p /docker/nginx-proxy/ssl
+mkdir -p /docker/nginx-proxy/vhost.d
+mkdir -p /docker/nginx-proxy/html 
 cd /docker/git_clone/nginx
 docker-compose pull
 docker-compose up -d
 
+mkdir -p /docker/rancher-server/mysql
 cd /docker/git_clone/rancher
 docker-compose pull
 docker-compose up -d
