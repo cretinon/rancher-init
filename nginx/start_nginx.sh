dc2976029c9c8900e60dbae0212090f48995debc
root <root@vps3.localdomain>
2017-11-12 06:42:03 +0000

    .

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -1,26 +1,52 @@
 #!/bin/sh
 
-docker run -d -p 80:80 -p 443:443 \
-  --name nginx \
-  -v /docker/nginx/conf.d:/etc/nginx/conf.d  \
-  -v /docker/nginx/vhost.d:/etc/nginx/vhost.d \
-  -v /docker/nginx/html:/usr/share/nginx/html \
-  -v /docker/nginx/certs:/etc/nginx/certs:ro \
-  --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy \
-  nginx
+docker swarm init
 
-docker run -d \
-  --name nginx-gen \
-  --volumes-from nginx \
-  -v /docker/git_clone/rancher-init/nginx/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
-  -v /var/run/docker.sock:/tmp/docker.sock:ro \
-  --label com.github.jrcs.letsencrypt_nginx_proxy_companion.docker_gen \
-  jwilder/docker-gen \
-  -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    --mount type=bind,src=/docker/portainer/dat,dst=/data \
+    portainer/portainer \
+    -H unix:///var/run/docker.sock
 
-docker run -d \
-  --name nginx-letsencrypt \
-  --volumes-from nginx \
-  -v /docker/nginx/certs:/etc/nginx/certs:rw \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  jrcs/letsencrypt-nginx-proxy-companion
+docker network create reverse-proxy
+
+mkdir /docker/nginx/conf.d
+mkdir /docker/nginx/vhost.d
+mkdir /docker/nginx/html
+mkdir /docker/nginx-proxy/ssl
+
+docker-compose up
+
+#docker run -d -p 80:80 -p 443:443 \
+#  --name nginx \
+#  --net reverse-proxy \
+#  --restart unless-stopped \
+#  -v /docker/nginx/conf.d:/etc/nginx/conf.d  \
+#  -v /docker/nginx/vhost.d:/etc/nginx/vhost.d \
+#  -v /docker/nginx/html:/usr/share/nginx/html \
+#  -v /docker/nginx/certs:/etc/nginx/certs:ro \
+#  --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy \
+#  nginx
+
+#docker run -d \
+#  --name nginx-gen \
+#  --net reverse-proxy \
+#  --restart unless-stopped \
+#  --volumes-from nginx \
+#  -v /docker/git_clone/rancher-init/nginx/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
+#  -v /var/run/docker.sock:/tmp/docker.sock:ro \
+#  --label com.github.jrcs.letsencrypt_nginx_proxy_companion.docker_gen \
+#  jwilder/docker-gen \
+#  -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
+
+#docker run -d \
+#  --name nginx-letsencrypt \
+#  --net reverse-proxy \
+#  --restart unless-stopped \
+#  --volumes-from nginx \
+#  -v /docker/nginx/certs:/etc/nginx/certs:rw \
+#  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+#  jrcs/letsencrypt-nginx-proxy-companion
--- setup_master.sh
+++ setup_master.sh
@@ -38,20 +38,24 @@ while [ $? -eq 1 ]; do sleep 10 ; mount /docker ; done
 mkdir -p /docker/git_clone
 cd /docker/git_clone
 git clone https://github.com/cretinon/rancher-init.git
-git clone https://github.com/rancher/compose-templates.git
+#git clone https://github.com/rancher/compose-templates.git
 
-mkdir -p /docker/bin
-cd /docker/bin
-wget https://github.com/rancher/rancher-compose/releases/download/v0.12.5/rancher-compose-linux-amd64-v0.12.5.tar.gz
-tar zxvf rancher-compose-linux-amd64-v0.12.5.tar.gz
-mv rancher-compose-v0.12.5/rancher-compose .
-rm -rf rancher-compose-linux-amd64-v0.12.5.tar.gz
-rm -rf rancher-compose-v0.12.5
+#mkdir -p /docker/bin
+#cd /docker/bin
+#wget https://github.com/rancher/rancher-compose/releases/download/v0.12.5/rancher-compose-linux-amd64-v0.12.5.tar.gz
+#tar zxvf rancher-compose-linux-amd64-v0.12.5.tar.gz
+#mv rancher-compose-v0.12.5/rancher-compose .
+#rm -rf rancher-compose-linux-amd64-v0.12.5.tar.gz
+#rm -rf rancher-compose-v0.12.5
+
+cp /docker/git_clone/rancher-init/docker.service /lib/systemd/system/docker.service
+systemctl daemon-reload
+systemctl  restart docker
 
 cd /docker/git_clone/rancher-init/nginx
 chmod +x start_nginx.sh
 ./start_nginx.sh
 
-cd /docker/git_clone/rancher-init/rancher
-chmod +x start_rancher.sh
-./start_rancher.sh
+#cd /docker/git_clone/rancher-init/rancher
+#chmod +x start_rancher.sh
+#./start_rancher.sh
