974d98c9696b0d8c6c11a2844ab67713fcd7b29c
root <root@vps3.localdomain>
2017-11-12 07:02:51 +0000

    .

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -2,12 +2,14 @@
 
 docker swarm init
 
+mkdir /docker/portainer/data
+
 docker service create \
     --name portainer \
     --publish 9000:9000 \
     --constraint 'node.role == manager' \
     --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
-    --mount type=bind,src=/docker/portainer/dat,dst=/data \
+    --mount type=bind,src=/docker/portainer/data,dst=/data \
     portainer/portainer \
     -H unix:///var/run/docker.sock
 
